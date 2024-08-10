e8957c182918ac859fa4643912c9f5145959b96f
Ahmed Hamed <ahmed.hamed@robustastudio.com>
2024-08-11 02:07:23 +0300

    refactor(home): use the src path alias

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import MyFirstComponent from "../components/MyFirstComponent";
+import MyFirstComponent from "@/components/MyFirstComponent";
 
 export default function Home() {
   return <MyFirstComponent pageName="Home" />;
